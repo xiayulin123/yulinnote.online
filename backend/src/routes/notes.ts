091a989a1d5b2556da51c90bdf89abb1fdf52815
xiayulin123 <xiayulinsky@gmail.com>
2023-07-22 17:22:33 -0400

    first commit

---
+++ backend/src/routes/notes.ts
@@ -0,0 +1,12 @@
+import * as NotesController from '../controllers/notes'
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/', NotesController.getNotes)
+router.get('/:noteId', NotesController.getNote)
+router.post('/', NotesController.createNote)
+router.patch('/:noteId', NotesController.updateNote)
+router.delete('/:noteId', NotesController.deleteNote)
+
+export default router
